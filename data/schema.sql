47986c387c8dff898eccec0476484be5cbd209d2
Ben Youngblood <bx.youngblood@gmail.com>
2012-05-15 16:44:19 -0700

    updated schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `user_role` (
-  `role_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `role_id` varchar(255) NOT NULL,
+  `default` tinyint(1) NOT NULL COMMENT 'set 1 for guest/default role',
   `weight` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`role_id`),
   KEY `weight` (`weight`)
