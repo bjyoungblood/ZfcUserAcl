d8ae780c026058fb3b9120cc85783ab29451208e
Ben Youngblood <bx.youngblood@gmail.com>
2012-05-23 13:59:20 -0700

    use the service manager instead of DI

--- Module.php
+++ Module.php
@@ -16,6 +16,11 @@ class Module extends ModuleAbstract implements
 {
     protected $zfcUserAclService;
 
+    public function init(ModuleManager $moduleManager)
+    {
+        $moduleManager->events()->attach('loadModules.post', array($this, 'modulesLoaded'));
+    }
+
     public function bootstrap(ModuleManager $manager, ApplicationInterface $app)
     {
         $locator = $app->getServiceManager();
@@ -28,8 +33,44 @@ class Module extends ModuleAbstract implements
     {
         return array(
             'factories' => array(
+                'zfcuseracl_db_adapter' => function($sm) {
+                    return $sm->get('Zend\Db\Adapter\Adapter');
+                },
+
+                'zfcuseracl_role_tg' => function($sm) {
+                    $adapter = $sm->get('zfcuseracl_db_adapter');
+                    $tg = new \Zend\Db\TableGateway\TableGateway('user_role', $adapter);
+                    return $tg;
+                },
+
+                'ZfcUserAcl\Model\RoleMapper' => function($sm) {
+                    $mapper = new Model\RoleMapper();
+                    $mapper->setTableGateway($sm->get('zfcuseracl_role_tg'));
+                    return $mapper;
+                },
+
+                'ZfcUserAcl\Service\RoleProvider' => function($sm) {
+                    return new Service\RoleProvider;
+                },
+
+                'ZfcUserAcl\Service\AclLoaderZendDb' => function ($sm) {
+                    $loader = new Service\AclLoaderZendDb;
+                    $loader->setRoleMapper($sm->get('ZfcUserAcl\Model\RoleMapper'));
+                    $loader->setUserService($sm->get('zfcuser_user_service'));
+                    return $loader;
+                },
+
+                'zfcacl_service' => function ($sm) {
+                    $aclService = $sm->get('ZfcAcl\Service\Acl');
+                    $aclService->setAclLoader($sm->get('ZfcUserAcl\Service\AclLoaderZendDb'));
+                    $aclService->setRoleProvider($sm->get('ZfcUserAcl\Service\RoleProvider'));
+                    return $aclService;
+                },
+
                 'ZfcUserAcl\Service\ZfcUserAclService' => function ($sm) {
                     $service = new Service\ZfcUserAclService;
+                    $service->setAclService($sm->get('zfcacl_service'));
+                    $service->setUserService($sm->get('zfcuser_user_service'));
                     return $service;
                 },
             ),
@@ -38,7 +79,8 @@ class Module extends ModuleAbstract implements
 
     public function modulesLoaded($e)
     {
-        $this->zfcUserAclService->init();
+        $service = $e->getParam('ServiceManager')->get('ZfcUserAcl\Service\ZfcUserAclService');
+        $service->load($e);
     }
 
     public function getDir()
@@ -50,5 +92,18 @@ class Module extends ModuleAbstract implements
     {
         return __NAMESPACE__;
     }
-     
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
 }
--- config/module.config.php
+++ config/module.config.php
@@ -1,44 +1,44 @@
 <?php
 return array(
-    'di' => array(
-        'instance' => array(
-            'alias' => array(
-                'zfcuser_role_mapper' => 'ZfcUserAcl\Model\RoleMapper',
-                'zfcuseracl_role_tg' => 'Zend\Db\TableGateway\TableGateway',
-            ),
-            'ZfcAcl\Service\Acl' => array(
-                'parameters' => array(
-                    'roleProvider' => 'ZfcUserAcl\Service\RoleProvider',
-                    'aclLoader' => 'ZfcUserAcl\Service\AclLoaderZendDb',
-                ),
-            ),
-            'ZfcUserAcl\Service\AclLoaderZendDb' => array(
-                'parameters' => array(
-                    'roleMapper' => 'zfcuser_role_mapper',
-                    'userService' => 'ZfcUser\Service\User',
-                    //'resourceMapper' => 'ZfcUserAcl\Model\ResourceMapper',
-                    //'ruleMapper' => 'ZfcUserAcl\Model\RuleMapper'
-                ),
-            ),
-            'ZfcUserAcl\Service\ZfcUserAclService' => array(
-                'parameters' => array(
-                    'aclService' => 'ZfcAcl\Service\Acl',
-                    'userService' => 'ZfcUser\Service\User',
-                    'roleMapper' => 'zfcuser_role_mapper',
-                ),
-            ),
+    //'di' => array(
+    //    'instance' => array(
+    //        'alias' => array(
+    //            'zfcuser_role_mapper' => 'ZfcUserAcl\Model\RoleMapper',
+    //            'zfcuseracl_role_tg' => 'Zend\Db\TableGateway\TableGateway',
+    //        ),
+    //        'ZfcAcl\Service\Acl' => array(
+    //            'parameters' => array(
+    //                'roleProvider' => 'ZfcUserAcl\Service\RoleProvider',
+    //                'aclLoader' => 'ZfcUserAcl\Service\AclLoaderZendDb',
+    //            ),
+    //        ),
+    //        'ZfcUserAcl\Service\AclLoaderZendDb' => array(
+    //            'parameters' => array(
+    //                'roleMapper' => 'zfcuser_role_mapper',
+    //                'userService' => 'ZfcUser\Service\User',
+    //                //'resourceMapper' => 'ZfcUserAcl\Model\ResourceMapper',
+    //                //'ruleMapper' => 'ZfcUserAcl\Model\RuleMapper'
+    //            ),
+    //        ),
+    //        'ZfcUserAcl\Service\ZfcUserAclService' => array(
+    //            'parameters' => array(
+    //                'aclService' => 'ZfcAcl\Service\Acl',
+    //                'userService' => 'ZfcUser\Service\User',
+    //                'roleMapper' => 'zfcuser_role_mapper',
+    //            ),
+    //        ),
 
-            'zfcuser_role_mapper' => array(
-                'parameters' => array(
-                    'tableGateway' => 'zfcuseracl_role_tg',
-                ),
-            ),
-            'zfcuseracl_role_tg' => array(
-                'parameters' => array(
-                    'table' => 'user_role',
-                    'adapter' => 'Zend\Db\Adapter\Adapter'
-                ),
-            ),
-        ),
-    ),
+    //        'zfcuser_role_mapper' => array(
+    //            'parameters' => array(
+    //                'tableGateway' => 'zfcuseracl_role_tg',
+    //            ),
+    //        ),
+    //        'zfcuseracl_role_tg' => array(
+    //            'parameters' => array(
+    //                'table' => 'user_role',
+    //                'adapter' => 'Zend\Db\Adapter\Adapter'
+    //            ),
+    //        ),
+    //    ),
+    //),
 );
